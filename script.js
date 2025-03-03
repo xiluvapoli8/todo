4837e2621277e98457d0fa1c597f67f6b02b975c
Xiluva Poli <xiluvapoli8@gmail.com>
2025-03-03 13:12:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 const tasks =[];
 
-function adder(){
-    const inputText = document.getElementById('').value;
+let divEmpty = document.getElementsByClassName('empty');
+let divNotEmpty = document.getElementsByClassName('not_empty);
 
+function chooseDisplay(){
+    if tasks.length===0{
+        divEmpty.style.display = 'none';
+        divNotEmpty.style.display = 'block';
+    };
+    else{
+        divEmpty.style.display = 'block';
+        divNotEmpty.style.display = 'none';
+    }
+}
+
+function adder(){
+    let inputText = document.getElementById('add_item').value;
+    if (inputText != ""){
+        tasks.push(inputText);
+        
+    }
+    else{
+        alert("Item cannot be empty!");
+    }
     
 }
